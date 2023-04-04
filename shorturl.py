5aff1c59e6612709a72bc7b6791a035a193ab353
Jack <92579700+JohnWickKeanue@users.noreply.github.com>
2023-04-04 21:22:55 +0530

    Update and rename shourturl.py to shorturl.py

--- shorturl.py
+++ shorturl.py
@@ -1,21 +1,20 @@
-
 import time
 import cloudscraper
 from bs4 import BeautifulSoup 
+import requests
 
-
-url = "https://urlsopen.net/ZuKH"  #@param {type:"string"}
+url = "https://urlsopen.com/ZuKH"  #@param {type:"string"}
 
 
 # leech with credits broo
 # ---------------------------------------------------------------------------------------------------------------------
 
-def shorturl(url):
+def shourturl(url):
     
     client = cloudscraper.create_scraper(allow_brotli=False)
     
     
-    DOMAIN = "https://blogpost.viewboonposts.com/go"
+    DOMAIN = "https://blogpost.viewboonposts.com/ssssssagasdgeardggaegaqe"
 
     url = url[:-1] if url[-1] == '/' else url
 
@@ -23,25 +22,25 @@ def shorturl(url):
     
     final_url = f"{DOMAIN}/{code}"
     
-    ref = "https://blog.textpage.xyz/"
+    ref = "https://blog.textpage.xyz/?safelink_redirect=eyJzZWNvbmRfc2FmZWxpbmtfdXJsIjoiIiwic2FmZWxpbmsiOiJodHRwczpcL1wvYmxvZy52aWV3Ym9vbnBvc3RzLmNvbVwvc2FmZS5waHA/bGluaz1cL3Nzc3Nzc2FnYXNkZ2VhcmRnZ2FlZ2FxZVwvZFNqUCJ9"
     
     h = {"referer": ref}
   
-    resp = client.get(final_url,headers=h)
-    
-    soup = BeautifulSoup(resp.content, "html.parser")
+    resp = client.get(final_url,headers=h).text
+   
+    soup = BeautifulSoup(resp, "html.parser")
     
     inputs = soup.find_all("input")
    
     data = { input.get('name'): input.get('value') for input in inputs }
 
     h = { "x-requested-with": "XMLHttpRequest" }
-    
     time.sleep(2)
     r = client.post(f"{DOMAIN}/links/go", data=data, headers=h)
     try:
-        return r.json()['url']
+        c = r.json()['url']
+        return c
     except: return "Something went wrong :("
     
 # ---------------------------------------------------------------------------------------------------------------------
-print(shorturl(url))
+print(shourturl(url))
